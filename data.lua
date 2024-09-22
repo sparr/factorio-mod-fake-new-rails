9c5ae98a9722cfe36c8d0cd1ab2f3bbbfc483d5b
Clarence "Sparr" Risher <sparr0@gmail.com>
2024-09-22 13:15:07 -0400

    Add elevated signals, v0.4.1

--- data.lua
+++ data.lua
@@ -71,29 +71,41 @@ local function elevate(entity)
   local picture = entity.picture--[[@as data.Sprite4Way.struct]]
   ---@type string, data.Sprite
   for _, sprite in pairs(picture--[=[@as table<string, data.Sprite>]=]) do
-    for layer_index, layer in pairs(sprite.layers) do
-      if layer_index ~= sprite_layer_indices["collisions"] then
-        if layer.shift ~= nil then
-          layer.shift[2] = layer.shift[2] - 3
-        else
-          layer.shift = {0, -3}
+    if sprite.layers then
+      for layer_index, layer in pairs(sprite.layers) do
+        if layer_index ~= sprite_layer_indices["collisions"] then
+          if layer.shift ~= nil then
+            layer.shift[2] = layer.shift[2] - 3
+          else
+            layer.shift = {0, -3}
+          end
         end
       end
-    end
-    orig_bottom_layer = 1
-    for i, layer_def in pairs(elevated_sprite_layers) do
-      local new_layer = table.deepcopy(sprite.layers[orig_bottom_layer])
-      new_layer.shift[2] = new_layer.shift[2] + (layer_def["yshift"] and layer_def["yshift"] or 0)
-      new_layer.draw_as_shadow = layer_def["shadow"]
-      new_layer.filename = new_layer.filename:gsub("entity/" .. sprite_layer_names[1] .. "%-", "entity/" .. ( layer_def["fileprefix"] or layer_def["name"] ).. "-")
-      if layer_def["bottom"] then
-        table.insert(sprite.layers, 1, new_layer)
-        orig_bottom_layer = orig_bottom_layer + 1
+      orig_bottom_layer = 1
+      for i, layer_def in pairs(elevated_sprite_layers) do
+        local new_layer = table.deepcopy(sprite.layers[orig_bottom_layer])
+        new_layer.shift[2] = new_layer.shift[2] + (layer_def["yshift"] and layer_def["yshift"] or 0)
+        new_layer.draw_as_shadow = layer_def["shadow"]
+        new_layer.filename = new_layer.filename:gsub("entity/" .. sprite_layer_names[1] .. "%-", "entity/" .. ( layer_def["fileprefix"] or layer_def["name"] ).. "-")
+        if layer_def["bottom"] then
+          table.insert(sprite.layers, 1, new_layer)
+          orig_bottom_layer = orig_bottom_layer + 1
+        else
+          table.insert(sprite.layers, sprite_layer_indices["collisions"], new_layer) -- collisions stay on top
+        end
+      end
+      sprite.layers[orig_bottom_layer].tint = {r=1, g=0.75, b=0.75, a=1}
+    else
+      if sprite.shift ~= nil then
+        if sprite.shift.y then
+          sprite.shift.y = sprite.shift.y - 3
+        else
+          sprite.shift[2] = sprite.shift[2] - 3
+        end
       else
-        table.insert(sprite.layers, sprite_layer_indices["collisions"], new_layer) -- collisions stay on top
+        sprite.shift = {x=0, y=-3}
       end
     end
-    sprite.layers[orig_bottom_layer].tint = {r=1, g=0.75, b=0.75, a=1}
   end
   entity.render_layer = "higher-object-above"
   -- not allowed in current Factorio
@@ -308,24 +320,30 @@ local signal_sprite_directions_info = {
 }
 
 -- signals
-for _, signal_info in pairs({{96, "rail-signal"}, {160, "rail-chain-signal"}}) do
-  local signal_sprite_size = signal_info[1]
-  local signal_name = signal_info[2]
-  name = "fake-" .. signal_name
-  icon = "__base__/graphics/icons/" .. signal_name .. ".png"
-  data:extend(get_recipe_and_item_prototypes(name, icon))
-  entity = get_entity_prototype(name, icon, {1,1}, {0.4, 0.4})
-  entity.build_grid_size = 1
-  for index, sprite_info in pairs(signal_sprite_directions_info) do
-    local sprite_direction = sprite_info[1]
-    entity.picture[sprite_direction] = {
-      filename = "__base__/graphics/entity/" .. signal_name .. "/" .. signal_name .. ".png",
-      width = signal_sprite_size,
-      height = signal_sprite_size,
-      x = signal_name == "rail-chain-signal" and 480 or 0, -- green chain signals in fourth column
-      y = (index - 1) * signal_sprite_size * 2,
-      shift = signal_name == "rail-chain-signal" and sprite_info[2] or nil -- chain signals are off center
-    }
+-- elevated and non-elevated versions
+for elevation_id, elevation_name in pairs({"lo", "hi"}) do
+  for _, signal_info in pairs({{96, "rail-signal"}, {160, "rail-chain-signal"}}) do
+    local signal_sprite_size = signal_info[1]
+    local signal_name = signal_info[2]
+    name = "fake-" .. signal_name .. "-" .. elevation_name
+    icon = "__base__/graphics/icons/" .. signal_name .. ".png"
+    data:extend(get_recipe_and_item_prototypes(name, icon))
+    entity = get_entity_prototype(name, icon, {1,1}, {0.4, 0.4})
+    entity.build_grid_size = 1
+    for index, sprite_info in pairs(signal_sprite_directions_info) do
+      local sprite_direction = sprite_info[1]
+      entity.picture[sprite_direction] = {
+        filename = "__base__/graphics/entity/" .. signal_name .. "/" .. signal_name .. ".png",
+        width = signal_sprite_size,
+        height = signal_sprite_size,
+        x = signal_name == "rail-chain-signal" and 480 or 0, -- green chain signals in fourth column
+        y = (index - 1) * signal_sprite_size * 2,
+        shift = signal_name == "rail-chain-signal" and table.deepcopy(sprite_info[2]) or nil -- chain signals are off center
+      }
+    end
+    if elevation_id == 2 then
+      elevate(entity)
+    end
+    data:extend({entity})
   end
-  data:extend({entity})
 end
